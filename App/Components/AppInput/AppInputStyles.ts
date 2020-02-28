20efa37ee63841abdc189e7470b066ab4535a722
João Picolo <jpp18@inf.ufpr.br>
2020-02-28 20:20:19 -0300

    Creates Components
    - Button
    - Input

---
+++ App/Components/AppInput/AppInputStyles.ts
@@ -0,0 +1,17 @@
+import { ViewStyle } from 'react-native';
+import {
+    widthPercentageToDP as wp,
+  } from 'react-native-responsive-screen';
+
+import appColors from '../../Themes/appColors';
+
+export default {
+    inputContainer: {
+        borderColor: appColors.darkgrey,
+        borderWidth: wp('0.3%'),
+        marginLeft: wp('15%'),
+        marginRight: wp('15%'),
+        padding: wp('2.5%'),
+        borderRadius: wp('3%')
+    } as ViewStyle
+}
